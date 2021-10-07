6f9903883e6fe0fdd52e4eb87447dca5c4cdf9b5
emilio-stack <84351829+emilio-stack@users.noreply.github.com>
2021-10-07 15:12:45 -0600

    created player class

---
+++ hilo/game/player.py
@@ -0,0 +1,39 @@
+class PLAYER:
+
+    def choose_hilo():
+        """This function asks the 
+        player to choose hi or lo"""
+
+        # Use a loop to repromt if user input is invalid.
+        loop_sentinel = True
+        while loop_sentinel:
+
+            # Prompt.
+            hi_or_lo = input("Higher or Lower? [h/l] ")
+
+            # Validate input
+            if hi_or_lo.lower() == "h" or hi_or_lo.lower() == "l":
+
+                # Return
+                return hi_or_lo
+
+            # Display error message.
+            else:
+                print("That input is not vaild. Try again.")
+
+    def play_again():
+        """This function asks if 
+        the player wants to play again."""
+
+        loop_sentinel = True
+        while loop_sentinel:
+
+            # Prompt.
+            answer = input("Keep playing? [y/n] ")
+
+            if answer.lower() == "y":
+                return True
+            elif answer.lower() == "n":
+                return False
+            else:
+                print("That input is not vaild. Try again.")
